8535a1898407c3ee5c05acdf6af6c62dfdb21ccb
Davestrekher <davigabrielli20005@gmail.com>
2026-08-19 21:25:37 -0300

    Esqueleto do autômato adicionado, mas ainda não testado

---
+++ Estado.java
@@ -0,0 +1,18 @@
+public class Estado{
+	private int id;
+	private boolean ehFinal;
+	private boolean ehInicial;
+
+	public int getId(){
+    return id;
+	}
+
+	public boolean getEhFinal(){
+    return ehFinal;
+	}
+
+	public boolean getEhInicial(){
+    return ehInicial;
+	}
+
+}
