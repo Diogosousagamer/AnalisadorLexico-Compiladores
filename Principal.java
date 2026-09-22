0d96cfe8dfa194418057973665adaa7705bfab41
Diogosousagamer <oliveira.desousa.dgamer@gmail.com>
2026-09-21 22:01:47 -0300

    Comentando classes pequenas

--- Principal.java
+++ Principal.java
@@ -21,19 +21,35 @@ import javafx.scene.text.Font;
 import javafx.stage.Stage;
 
 public class Principal extends Application {
+
+	/*
+     * ***************************************************************
+     * Metodo: start
+     * Funcao: configura a aplicacao
+     * Parametros: Stage stage - janela a ser configurada
+     * Retorno: void
+     ****************************************************************/
+
 	@Override
 	public void start(Stage stage) throws IOException {
+		// Carrega os controllers para evitar erros de compilacao
 		TelaMenuController TelaMenuController = new TelaMenuController();
 		TelaPrincipalController TelaPrincipalController = new TelaPrincipalController();
 
+		// Carrega a cena do programa
 		Parent root = FXMLLoader.load(getClass().getResource("/view/TelaMenu.fxml"));
 		Scene scene = new Scene(root);
 
+		// Carrega o CSS do programa
 		scene.getStylesheets().add(getClass().getResource("util/menu.css").toExternalForm());
+
+		// Carrega a fonte "VCR OSD Mono"
 		Font.loadFont(getClass().getResourceAsStream("/util/VCR_OSD_MONO_1.001.ttf"), 18);
 
+		// Carrega o icone do programa
 		Image icon = new Image(getClass().getResource("/img/compiler.png").toExternalForm());
 
+		// Configura a janela
 		stage.setTitle("Compilador - Mini Pascal");
 		stage.getIcons().add(icon);
 		stage.setScene(scene);
@@ -41,6 +57,14 @@ public class Principal extends Application {
 		stage.setMaximized(true);
 		stage.show();
 	}
+
+	/*
+     * ***************************************************************
+     * Metodo: main
+     * Funcao: executa a aplicacao
+     * Parametros: String[] args - vetor de argumentos
+     * Retorno: void
+     ****************************************************************/
 
 	public static void main(String[] args) {
 		launch(args);
--- model/LerArquivo.java
+++ model/LerArquivo.java
@@ -20,54 +20,103 @@ public class LerArquivo {
     // Arquivo a ser lido
 	private File arquivo;
 
+    /*
+     * ***************************************************************
+     * Metodo: LerArquivo
+     * Funcao: inicializa uma nova instancia da classe LerArquivo
+     * Parametros: File arquivo - arquivo a ser lido
+     * Retorno: nenhum
+     ****************************************************************/
+
 	public LerArquivo(File arquivo) {
 		this.arquivo = arquivo;
 	}
 
+    /*
+     * ***************************************************************
+     * Metodo: lerArquivo
+     * Funcao: le e retorna uma nova linha do arquivo
+     * Parametros: int linha - indice da linha a ser lida
+     * Retorno: String
+     ****************************************************************/
+
 	public String lerArquivo(int linha) {
+       // Variavel reservada para guardar o resultado
        String res = "";
        
+       // Tenta-se executar o seguinte codigo
        try {
+           // Contador de linhas
            int acc = 0;
+
+           // Abre-se o scanner para varrer o arquivo
            Scanner sc = new Scanner(arquivo);
 
+           // Enquanto o arquivo ainda tiver linhas
            while (sc.hasNextLine()) {
+               // Se o acumulador apontar pra linha procurada
                if (acc == linha) {
+                   // Armazena a linha atual e sai do laco
                    res = sc.nextLine();
                    break;
                }
                
+               // Caso nenhuma correspondencia for encontrada, 
+               // passa pra proxima linha
                sc.nextLine();
+
+               // Incrementa o contador
                acc++;
             }
 
-           sc.close();
+            // Fecha o scanner
+            sc.close();
         } 
        catch (FileNotFoundException ex) {
+            // Em caso de excecao, sinaliza que o arquivo nao foi encontrado
             System.out.println("arquivo nao encontrado");
        }
 
+       // Retorna a linha obtida
        return res; 
     }
 
-  public Integer tamanhoArquivo() {
-     int tamanho = 0;
-     
-     try {
-        Scanner sc = new Scanner(arquivo);
-        
-        while (sc.hasNextLine()) {
-            tamanho++;
-            sc.nextLine();
-        }
-        
-        sc.close();
-     } 
-     catch (FileNotFoundException ex) {
-        System.out.println("arquivo nao encontrado");
-     }
+    
+  /*
+   * ***************************************************************
+   * Metodo: tamanhoArquivo
+   * Funcao: retorna o tamanho do arquivo (em termos de linhas)
+   * Parametros: nenhum parametro foi definido para esta funcao
+   * Retorno: Integer
+   ****************************************************************/
 
-     System.out.println(tamanho);
-     return tamanho;
-  }
+    public Integer tamanhoArquivo() {
+        // Contador do tamanho
+        int tamanho = 0;
+     
+        // O metodo tenta executar este bloco de codigo
+        try {
+            // Abre o scanner para varrer o arquivo
+            Scanner sc = new Scanner(arquivo);
+
+            while (sc.hasNextLine()) {
+                // Incrementa o tamanho enquanto houverem linhas a serem computadas
+                tamanho++;
+                sc.nextLine();
+            }
+
+            // Fecha o scanner depois que a varredura foi concluida
+            sc.close();
+        } 
+        catch (FileNotFoundException ex) {
+            // Em caso de excecao, sinaliza que o arquivo nao foi encontrado
+            System.out.println("arquivo nao encontrado");
+        }
+
+        // Imprime o tamanho (quantidade de linhas) do arquivo
+        System.out.println(tamanho);
+
+        // Retorna o tamanho do arquivo
+        return tamanho;
+    }
 }
